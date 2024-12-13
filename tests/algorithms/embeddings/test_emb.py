Update add gradient clip to fix bug

tests/algorithms/embeddings/test_emb.py
@@ -101,7 +101,7 @@ def check_freeze(emb_layer: BaseTokenEmbeddingLayer, tokenizer: AbstractTokenize
     # test unfreeze
     record0 = emb_layer.embedding.weight.data.sum().item()
 
-    optimizer = torch.optim.SGD(net.parameters(), lr=1e-1)
+    optimizer = torch.optim.SGD(net.parameters(), lr=1e-2)
 
     for _ in range(10):
         output = net(src)
@@ -109,6 +109,9 @@ def check_freeze(emb_layer: BaseTokenEmbeddingLayer, tokenizer: AbstractTokenize
         loss = ((labels - output) ** 2).sum()
         optimizer.zero_grad()
         loss.backward()
+
+        torch.nn.utils.clip_grad_value_(net.parameters(), clip_value=0.1)
+        
         optimizer.step()
 
     assert not emb_layer.is_frozen
@@ -120,12 +123,16 @@ def check_freeze(emb_layer: BaseTokenEmbeddingLayer, tokenizer: AbstractTokenize
     emb_layer.freeze_parameters()
     assert emb_layer.is_frozen
 
-    output = net(src)
-    assert output.shape == (batch_size, seq_length, out_features)
-    loss = ((labels - output) ** 2).sum()
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    for _ in range(10):
+        output = net(src)
+        assert output.shape == (batch_size, seq_length, out_features)
+        loss = ((labels - output) ** 2).sum()
+        optimizer.zero_grad()
+        loss.backward()
+
+        torch.nn.utils.clip_grad_value_(net.parameters(), clip_value=0.1)
+
+        optimizer.step()
 
     record2 = emb_layer.embedding.weight.data.sum().item()
 